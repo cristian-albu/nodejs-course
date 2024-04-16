ad069fb6077b98f66b438cb8f77474c231078971
cristian-albu <albucristian95p@gmail.com>
2024-04-16 21:54:52 +0300

    web servers

---
+++ 06-express-app/models/friends.model.js
@@ -0,0 +1,16 @@
+const friends = [
+  {
+    id: 0,
+    name: "Isaac Newton",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+  {
+    id: 2,
+    name: "Oppenheimer",
+  },
+];
+
+module.exports = friends;
